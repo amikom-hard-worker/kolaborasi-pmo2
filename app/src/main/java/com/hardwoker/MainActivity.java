33861477089a824d20f09bb4b4b99a8432f383d7
aldiaprilianto <aldiapriliantogalur.234@gmail.com>
2021-01-13 18:40:58 +0700

    Tambah produk (database) belum tampilan

--- app/src/main/java/com/hardwoker/MainActivity.java
+++ app/src/main/java/com/hardwoker/MainActivity.java
@@ -4,19 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.hardwoker.adapter.CategoryAdapter;
 import com.hardwoker.adapter.DiscountedProductAdapter;
+import com.hardwoker.database.DatabaseTB;
 import com.hardwoker.model.Category;
 import com.hardwoker.model.DiscountedProducts;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView discountRecylerView, categoryRecyclerView;
+    FloatingActionButton tblData;
+    RecyclerView recyclerView;
+
 
     DiscountedProductAdapter discountedProductAdapter;
     List<DiscountedProducts> discountedProductsList;
@@ -28,6 +48,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //tambah data
+        tblData = findViewById(R.id.tbl_data);
+        recyclerView = findViewById(R.id.productRecycler);
+
+        tblData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogTambahData();
+            }
+        });
 
         //recylerview untuk diskon
         discountRecylerView = findViewById(R.id.discountedRecycler);
@@ -59,6 +90,60 @@ public class MainActivity extends AppCompatActivity {
         setCategoryRecycler(categoryList);
 
 
+    }
+    // tambah data produk
+    private void showDialogTambahData() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.tambah_data);
+
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(true);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(Objects.requireNonNull(dialog.getWindow()).getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog.getWindow().setAttributes(lp);
+
+        ImageButton tblKeluar = dialog.findViewById(R.id.tbl_keluar);
+        tblKeluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        final EditText txtTambah = dialog.findViewById(R.id.txt_tambah);
+        final Button tblTambah = dialog.findViewById(R.id.tbl_tambah);
+
+        tblTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(txtTambah.getText())){
+                    tblTambah.setError("Silahkan Isi Data");
+                }else {
+                    simpanData(txtTambah.getText().toString());
+                    dialog.dismiss();
+                }
+            }
+
+
+        });
+        dialog.show();
+    }
+    //simpan data produk
+    private void simpanData(String s) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        String kunci = myRef.push().getKey();
+        DatabaseTB dataTB = new DatabaseTB(kunci, s);
+
+        myRef.child(kunci).setValue(dataTB).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     // bagian discon
---
+++ app/src/main/java/com/hardwoker/database/DatabaseTB.java
@@ -0,0 +1,26 @@
+package com.hardwoker.database;
+
+public class DatabaseTB {
+    String kunci;
+    String isi;
+
+    public DatabaseTB(String kunci, String isi){
+        this.kunci = kunci;
+        this.isi = isi;
+    }
+    public String getKunci() {
+        return kunci;
+    }
+
+    public void setKunci(String kunci) {
+        this.kunci = kunci;
+    }
+
+    public String getIsi() {
+        return isi;
+    }
+
+    public void setIsi(String isi) {
+        this.isi = isi;
+    }
+}
