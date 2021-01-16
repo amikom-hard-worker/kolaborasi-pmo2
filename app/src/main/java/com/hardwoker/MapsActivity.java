7984766c9cef9e9d2b6e14856f07f2ee2f719324
arispuu <arispurnomo300@gmail.com>
2021-01-16 11:03:46 +0700

    fitur map toko terdekat

--- app/src/main/java/com/hardwoker/MapsActivity.java
+++ app/src/main/java/com/hardwoker/MapsActivity.java
@@ -2,7 +2,22 @@ package com.hardwoker;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,7 +26,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback , GoogleMap.OnMyLocationButtonClickListener
+        , GoogleMap.OnMyLocationClickListener {
 
     private GoogleMap mMap;
 
@@ -23,6 +39,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        final EditText etNama = findViewById(R.id.etnama);
+        Button btnLokasi = findViewById(R.id.btnlokasi);
+        btnLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntertUri = Uri.parse("geo:0,0?q=" + etNama.getText().toString());
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntertUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
+
+        Button btnKriteria = findViewById(R.id.btnterdekat);
+        btnKriteria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String kriteria = "toko bangunan terdekat";
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + kriteria);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
+
     }
 
     /**
@@ -39,8 +82,67 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng alunpwt = new LatLng(-7.424414, 109.230235);
-        mMap.addMarker(new MarkerOptions().position(alunpwt).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(alunpwt));
+        LatLng alunPwt = new LatLng(-7.424431,109.230235);
+        mMap.addMarker(new MarkerOptions().position(alunPwt).title("Alun alun purwokerto"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(alunPwt));
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new
+                                String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                                , Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1);
+            } else {
+                mMap.setMyLocationEnabled(true);
+            }
+        } else {
+            mMap.setMyLocationEnabled(true);
+        }
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull
+            String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted",
+                        Toast.LENGTH_SHORT).show();
+                if (ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+                }
+            } else {
+                Toast.makeText(this, "Permission Denied",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "My Location Button Clicked",
+                Toast.LENGTH_SHORT).show();
+        return true;
+    }
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Lokasiku saat ini : " + location,
+                Toast.LENGTH_LONG).show();
+    }
+
+
+    public void pindah(View view) {
+        Intent intent = new Intent(MapsActivity.this, AllCategory.class);
+        startActivity(intent);
     }
 }
